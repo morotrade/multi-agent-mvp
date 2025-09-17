53bbdf65076ff74722200fd1188f98cb673b1a7f
LorenzoMorabito <lorenzomorabito.94@gmail.com>
2025-09-17 22:43:02 +0200

    chore: align agents + workflows + requirements

--- .github/agents/reviewer.py
+++ .github/agents/reviewer.py
@@ -19,8 +19,8 @@ from typing import Optional, List, Dict
 # Import segmented modules
 from rew_core import ProjectDetector, LLMReviewer, CommentManager
 from rew_policies import LabelManager, PolicyEnforcer
-from .utils.github_api import get_repo_info, get_pr, get_pr_files
-from .state import thread_ledger
+from utils.github_api import get_repo_info, get_pr, get_pr_files
+from state import ThreadLedger
 
 def get_pr_number_from_env() -> int:
     """Get PR number from environment or GitHub event"""
